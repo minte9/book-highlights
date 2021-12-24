4c50d8569ff4c7351e16bd0a520b10e6dd3a4d32
minte9 <catalinprescure@gmail.com>
2021-12-24 19:00:07 +0200

    update

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ console.log('\n'.repeat(25) + sep + '\n' + ' '.repeat(2)
     + chalk.greenBright(author.name + ', ')
     + chalk.redBright(book.title)
     + '\n' + sep
-    + '\n' +
+    + '\n'
     + chalk.bgGrey.black(start)
     + chalk.bgYellowBright.black(highlighted)
     + chalk.bgGrey.black(end)
