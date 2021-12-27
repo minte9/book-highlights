aa66c7412ee0d1fdcb6b07d84bb9f7e98b8aabf5
minte9 <catalinprescure@gmail.com>
2021-12-27 12:30:19 +0200

    update

--- public_html/utils/update_gui.js
+++ public_html/utils/update_gui.js
@@ -45,9 +45,14 @@ function get_rand(seed=true) {
 
 function update_gui() {
 
+	let title = obj.highlight.name.split('.txt')[0];
+	title = title.replace(/_/g, ' ');
+	title = title.replace(/\d/g, ' ');
+
 	$('#bh-m9-text').html(obj.highlight.text);
 	$('#bh-m9-wiki').attr({'href': obj.author.wiki, 'title': obj.author.tags});
-	$('#bh-m9-author').text(obj.author.name + ", ");
+	$('#bh-m9-author').text(obj.author.name);
+	$('#bh-m9-title').text(' / ' + title);
 	$('#bh-m9-reference').attr({'href': obj.book.link, title: obj.book.subtitle});
 	$('#bh-m9-book').text(obj.book.title);
 }
