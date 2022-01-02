665544df276f09cc1de732caf7254efb8da1ac01
minte9 <catalinprescure@gmail.com>
2022-01-02 15:33:30 +0200

    upgrade

--- index.js
+++ index.js
@@ -3,49 +3,49 @@
 /**
  * Get one random book highlight
  */
- import chalk from 'chalk';
- import { fileURLToPath } from 'url';
- import { dirname } from 'path';
- import { createRequire } from "module";
- 
- const __filename = fileURLToPath(import.meta.url);
- const __dirname = dirname(__filename);
- const require = createRequire(import.meta.url);
- 
- let AUTHORS     = require(__dirname + '/public_html/data//authors.json');
- let BOOKS       = require(__dirname + '/public_html/data//books.json');
- let HIGHLIGHTS  = require(__dirname + '/public_html/data/highlights.json');
- 
- const rand = Math.random();
- const i = Math.floor(rand * BOOKS.length);
- const book = BOOKS[i];
- 
- HIGHLIGHTS = HIGHLIGHTS.filter(x => x.name == book.title);
- HIGHLIGHTS = HIGHLIGHTS[0].children;
- 
- const author = AUTHORS.filter(x => x.name == HIGHLIGHTS[0].name)[0];
- 
- HIGHLIGHTS = HIGHLIGHTS[0].children;
- const j = Math.floor(rand * HIGHLIGHTS.length);
- const highlight = HIGHLIGHTS[j];
- 
- highlight.text = ' '.repeat(2) + highlight.text; 
- highlight.text = highlight.text.replace(/\n/g, (' '.repeat(2) + '\n') + ' '.repeat(2));
- 
- const splits = highlight.text.split(/<\/?i>/);
- const start = splits[0];
- const highlighted = ' ' + splits[1] + ' ';
- const end = splits[2];
- const sep = chalk.grey('-'.repeat(60));
- 
- console.log('\n'.repeat(25) + sep + '\n' + ' '.repeat(2)
-     + chalk.greenBright(author.name + ', ')
-     + chalk.redBright(book.title)
-     + '\n' + sep
- );
- console.log(''
-     + chalk.bgGrey.black(start)
-     + chalk.bgYellowBright.black(highlighted)
-     + chalk.bgGrey.black(end)
-     + '\n' + sep + '\n'.repeat(2)
- );
+import chalk from 'chalk';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createRequire } from "module";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const require = createRequire(import.meta.url);
+
+let AUTHORS     = require(__dirname + '/public_html/data//authors.json');
+let BOOKS       = require(__dirname + '/public_html/data//books.json');
+let HIGHLIGHTS  = require(__dirname + '/public_html/data/highlights.json');
+
+const rand = Math.random();
+const i = Math.floor(rand * BOOKS.length);
+const book = BOOKS[i];
+
+HIGHLIGHTS = HIGHLIGHTS.filter(x => x.name == book.title);
+HIGHLIGHTS = HIGHLIGHTS[0].children;
+
+const author = AUTHORS.filter(x => x.name == HIGHLIGHTS[0].name)[0];
+
+HIGHLIGHTS = HIGHLIGHTS[0].children;
+const j = Math.floor(rand * HIGHLIGHTS.length);
+const highlight = HIGHLIGHTS[j];
+
+highlight.text = ' '.repeat(2) + highlight.text; 
+highlight.text = highlight.text.replace(/\n/g, (' '.repeat(2) + '\n') + ' '.repeat(2));
+
+const splits = highlight.text.split(/<\/?i>/);
+const start = splits[0];
+const highlighted = ' ' + splits[1] + ' ';
+const end = splits[2];
+const sep = chalk.grey('-'.repeat(60));
+
+console.log('\n'.repeat(25) + sep + '\n' + ' '.repeat(2)
+    + chalk.greenBright(author.name + ', ')
+    + chalk.redBright(book.title)
+    + '\n' + sep
+);
+console.log(''
+    + chalk.bgGrey.black(start)
+    + chalk.bgYellowBright.black(highlighted)
+    + chalk.bgGrey.black(end)
+    + '\n' + sep + '\n'.repeat(2)
+);
