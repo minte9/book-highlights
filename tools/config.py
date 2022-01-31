20655b7bf47d4225d2308420c5ef2fc33122d58c
minte9 <catalinprescure@gmail.com>
2022-01-31 20:33:33 +0200

    upgrade

--- tools/config.py
+++ tools/config.py
@@ -5,4 +5,4 @@ DIR = pathlib.Path(__file__).resolve().parent
 
 cfp = configparser.ConfigParser()
 cfp.read(DIR / '../config/config_m9.ini') 
-# cfp.read(DIR / '../config/config_my.ini') # LOOK HERE
+    # cfp.read(DIR / '../config/config_my.ini') # LOOK HERE
