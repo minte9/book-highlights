2dde668c752c9f01f92d114d12be5b56d24a02b2
minte9 <catalinprescure@gmail.com>
2022-02-06 10:08:26 +0200

    upgrade

--- update.py
+++ update.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
+"""
+Your highlights:
+
+1) Copy config.sample.ini content to config.ini file
+2) Save screenshots in .png format to my_books/ folder
+3) Install python libraries: tesseract & opencv
+4) Run update script
+
+cd book-highlights/
+
+pip install --user opencv-python
+pip show opencv-python
+
+sudo apt-get update
+sudo apt-get install tesseract-ocr
+sudo apt-get install libtesseract-dev
+
+pip install pytesseract
+pip show pytesseract
+
+python update.py
+node index.js
+"""
+
 import sys
 sys.dont_write_bytecode = True
 
