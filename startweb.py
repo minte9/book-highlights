23af2f80298056295d72904ae420333bbbcb46b2
minte9 <catalinprescure@gmail.com>
2022-12-24 19:46:12 +0200

    upgrade

--- startweb.py
+++ startweb.py
@@ -7,8 +7,8 @@ python startweb.py
     # Ctrl+C to kill the process
 
 Alternatives
-    php -S localhost:8000
-    python3 -m http.server 8000 
+    php -S localhost:8080
+    python -m http.server --bind localhost 8080
     http://localhost:8000/public_html
 """
 
@@ -19,8 +19,14 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 DIR = pathlib.Path(__file__).resolve().parent
 os.chdir(DIR)
 
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Headers', 'cache-control')
+        SimpleHTTPRequestHandler.end_headers(self)
+
 def start_server():
-    httpd = HTTPServer(('127.0.0.1', 8080), SimpleHTTPRequestHandler)
+    httpd = HTTPServer(("", 8080), CORSRequestHandler)
     httpd.serve_forever()
 
 threading.Thread(target=start_server).start()
